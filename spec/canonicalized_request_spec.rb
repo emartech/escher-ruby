0d2842910d18fef2cbbf75ad2be43baef439d6dd
csaba.simon <csaba.simon@emarsys.com>
2014-08-12 18:08:37 +0200

    [SUITEDEV-1992] reorder results

--- spec/canonicalized_request_spec.rb
+++ spec/canonicalized_request_spec.rb
@@ -6,7 +6,7 @@ fixtures = %w(get-vanilla-query post-x-www-form-urlencoded)
 describe 'Escher' do
   fixtures.each do |test|
     it "should calculate canonicalized request for #{test}" do
-        body, date, headers, method, url = read_request(test)
+        method, url, body, date, headers = read_request(test)
         headers_to_sign = headers.keys.map(&:downcase)
         canonicalized_request = Escher.new.canonicalize method, url, body, date, headers, headers_to_sign
         expect(canonicalized_request).to eq(fixture(test, 'creq'))
@@ -26,5 +26,5 @@ def read_request(test)
 
   body = lines[-1]
   date = headers['Date']
-  return body, date, headers, method, url
+  return method, url, body, date, headers
 end
