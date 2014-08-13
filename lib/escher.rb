766e98e0848eca535360f84d7c77bcb59b0e97e7
csaba.simon <csaba.simon@emarsys.com>
2014-08-13 16:32:04 +0200

    [SUITEDEV-1992] parse auth header and validate signature

--- lib/escher.rb
+++ lib/escher.rb
@@ -5,20 +5,53 @@ require 'digest'
 class Escher
   VERSION = '0.0.1'
 
-  def get_auth_header(header_name, vendor_prefix, algo, api_key_id, api_secret, date, credential_scope, method, url, body, headers, signed_headers)
-    canonicalized_request = canonicalize method, url, body, date, headers, signed_headers
+  def validate_request(method, url, body, headers, auth_header_name, date_header_name, vendor_prefix)
+    auth_header = get_header(auth_header_name, headers)
+    date = get_header(date_header_name, headers)
+
+    algo, api_key_id, short_date, credential_scope, signed_headers, signature = parse_auth_header auth_header, vendor_prefix
+
+    api_secret = 'wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY'
+
+    signature == expected_signature(algo, api_secret, body, credential_scope, date, headers, method, signed_headers, url, vendor_prefix, auth_header_name)
+  end
+
+  def expected_signature(algo, api_secret, body, credential_scope, date, headers, method, signed_headers, url, vendor_prefix, auth_header_name)
+    generate_signature(algo, api_secret, body, credential_scope, date, headers, method, signed_headers, url, vendor_prefix, auth_header_name)
+  end
+
+  def get_header(header_name, headers)
+    (headers.detect { |header| header[0].downcase == header_name.downcase })[1]
+  end
+
+  def parse_auth_header(auth_header, vendor_prefix)
+    m = /#{vendor_prefix.upcase}-HMAC-(?<algo>[A-Z0-9\,]+) Credential=(?<credentials>[A-Za-z0-9\/\-_]+), SignedHeaders=(?<signed_headers>[A-Za-z\-;]+), Signature=(?<signature>[0-9a-f]+)$/
+      .match auth_header
+    [
+        m['algo'],
+    ] + m['credentials'].split('/', 3) + [
+        m['signed_headers'].split(';'),
+        m['signature'],
+    ]
+  end
+
+  def get_auth_header(auth_header_name, vendor_prefix, algo, api_key_id, api_secret, date, credential_scope, method, url, body, headers, signed_headers)
+    signature = generate_signature(algo, api_secret, body, credential_scope, date, headers, method, signed_headers, url, vendor_prefix, auth_header_name)
+    "#{algo_id(vendor_prefix, algo)} Credential=#{api_key_id}/#{long_date(date)[0..7]}/#{credential_scope}, SignedHeaders=#{signed_headers.uniq.join ';'}, Signature=#{signature}"
+  end
+
+  def generate_signature(algo, api_secret, body, credential_scope, date, headers, method, signed_headers, url, vendor_prefix, auth_header_name)
+    canonicalized_request = canonicalize method, url, body, date, headers, signed_headers, algo, auth_header_name
     string_to_sign = get_string_to_sign credential_scope, canonicalized_request, date, vendor_prefix, algo
     signing_key = calculate_signing_key(api_secret, date, vendor_prefix, credential_scope, algo)
     signature = calculate_signature(algo, signing_key, string_to_sign)
-
-    "#{algo_id(vendor_prefix, algo)} Credential=#{api_key_id}/#{long_date(date)[0..7]}/#{credential_scope}, SignedHeaders=#{signed_headers.uniq.join ';'}, Signature=#{signature}"
   end
 
   def calculate_signature(algo, signing_key, string_to_sign)
     Digest::HMAC.hexdigest(string_to_sign, signing_key, create_algo(algo))
   end
 
-  def canonicalize(method, url, body, date, headers, headers_to_sign = [], algo = 'SHA256')
+  def canonicalize(method, url, body, date, headers, headers_to_sign, algo, auth_header_name)
     url, query = url.split '?', 2 # URI#parse cannot parse unicode characters in query string TODO use Adressable
     uri = URI.parse(url)
 
@@ -26,7 +59,7 @@ class Escher
         method.upcase,
         canonicalize_path(uri),
         canonicalize_query(query),
-    ] + canonicalize_headers(date, uri, headers) + [
+    ] + canonicalize_headers(date, uri, headers, auth_header_name) + [
         '',
         (headers_to_sign | %w(date host)).join(';'),
         request_body_hash(body, algo)
@@ -81,18 +114,21 @@ class Escher
       path = path.gsub(%r{/\./}, '/').sub(%r{/\.\z}, '/').gsub(/\/+/, '/')
     end
 
-    def canonicalize_headers(date, uri, raw_headers)
-      collect_headers(raw_headers).merge({'date' => [date], 'host' => [uri.host]}).map { |k, v| k + ':' + (v.sort_by { |x| x }).join(',').gsub(/\s+/, ' ').strip }
+    def canonicalize_headers(date, uri, raw_headers, auth_header_name)
+      collect_headers(raw_headers, auth_header_name).merge({'date' => [date], 'host' => [uri.host]}).map { |k, v| k + ':' + (v.sort_by { |x| x }).join(',').gsub(/\s+/, ' ').strip }
     end
 
-    def collect_headers(raw_headers)
+    def collect_headers(raw_headers, auth_header_name)
       headers = {}
       raw_headers.each { |raw_header|
-        if headers[raw_header[0].downcase] then
-          headers[raw_header[0].downcase] << raw_header[1]
-        else
-          headers[raw_header[0].downcase] = [raw_header[1]]
-        end }
+        if raw_header[0].downcase != auth_header_name.downcase then
+          if headers[raw_header[0].downcase] then
+            headers[raw_header[0].downcase] << raw_header[1]
+          else
+            headers[raw_header[0].downcase] = [raw_header[1]]
+          end
+        end
+      }
       headers
     end
 
--- spec/canonicalized_request_spec.rb
+++ spec/canonicalized_request_spec.rb
@@ -38,7 +38,7 @@ describe 'Escher' do
     it "should calculate canonicalized request for #{test}" do
       method, url, body, date, headers = read_request(test)
       headers_to_sign = headers.map {|k| k[0].downcase }
-      canonicalized_request = Escher.new.canonicalize method, url, body, date, headers, headers_to_sign
+      canonicalized_request = Escher.new.canonicalize method, url, body, date, headers, headers_to_sign, 'SHA256', 'Authorization'
       check_canonicalized_request(canonicalized_request, test)
     end
   end
@@ -47,7 +47,7 @@ describe 'Escher' do
     it "should calculate string to sign for #{test}" do
       method, url, body, date, headers = read_request(test)
       headers_to_sign = headers.map {|k| k[0].downcase }
-      canonicalized_request = Escher.new.canonicalize method, url, body, date, headers, headers_to_sign
+      canonicalized_request = Escher.new.canonicalize method, url, body, date, headers, headers_to_sign, 'SHA256', 'Authorization'
       string_to_sign = Escher.new.get_string_to_sign 'us-east-1/host/aws4_request', canonicalized_request, date, 'AWS4', 'SHA256'
                                                      expect(string_to_sign).to eq(fixture(test, 'sts'))
     end
@@ -60,6 +60,14 @@ describe 'Escher' do
       auth_header = Escher.new.get_auth_header 'Authorization', 'AWS4', 'SHA256', 'AKIDEXAMPLE', 'wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY', date, 'us-east-1/host/aws4_request', method, url, body, headers, headers_to_sign
       expect(auth_header).to eq(fixture(test, 'authz'))
     end
+  end
+
+  it 'should validate request' do
+    headers = [
+        ['Date', 'Mon, 09 Sep 2011 23:36:00 GMT'],
+        ['Authorization', 'AWS4-HMAC-SHA256 Credential=AKIDEXAMPLE/20110909/us-east-1/host/aws4_request, SignedHeaders=date;host, Signature=b27ccfbfa7df52a200ff74193ca6e32d4b48b8856fab7ebf1c595d0670a7e470']
+    ]
+    expect(Escher.new.validate_request 'GET', 'http://host.foo.com/', '', headers, 'Authorization', 'Date', 'AWS4').to be true
   end
 end
 
@@ -75,8 +83,8 @@ def get_date(headers)
   headers.detect {|header| k, v = header; k.downcase == 'date'}[1]
 end
 
-def read_request(test)
-  lines = (fixture(test, 'req') + "\n").lines.map(&:chomp)
+def read_request(test, extension = 'req')
+  lines = (fixture(test, extension) + "\n").lines.map(&:chomp)
   method, uri = lines[0].split ' '
   headers = lines[1..-3].map { |header| k, v = header.split(':', 2); [k, v] }
   url = 'http://'+ get_host(headers) + uri
