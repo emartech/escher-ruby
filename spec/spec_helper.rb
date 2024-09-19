94ef37780899d7556f71abdd475186fe0412edc8
potato <mail@potato.hu>
2024-09-19 10:41:27 +0200

    SECURITY-9850: fix deprecation warning
    Co-authored-by: Krisztian Nagy <krisztian.nagy@emarsys.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,5 @@ require 'escher'
 
 autoload :EmarsysTestSuiteHelpers, 'helpers/emarsys_test_suite_helpers'
 RSpec.configure do |c|
-  c.treat_symbols_as_metadata_keys_with_true_values = true
   c.extend EmarsysTestSuiteHelpers, :emarsys_test_suite
 end
